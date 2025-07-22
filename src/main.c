e3d3e6e560703af7159c29bdcd57fed5fc39ee9c
roelof <roelof.hol@gmail.com>
2025-07-22 15:46:35 +0200

    bit more restructuring & turn mlx back on

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: rhol <rhol@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/06/02 17:05:48 by rhol          #+#    #+#                 */
-/*   Updated: 2025/07/22 15:28:18 by roelof        ########   odam.nl         */
+/*   Updated: 2025/07/22 15:45:49 by roelof        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,24 @@
 
 // maybe put draw_hook into game_hook cause smol.
 // limit fps here ?
-// void	draw_hook(void *param)
-// {
-// 	t_vars *data;
+void	draw_hook(void *param)
+{
+	t_vars *data;
 
-// 	data = (t_vars *)param;
-// 	draw_3d_view(data);
-// 	draw_small_minimap(data);
-// }
+	data = (t_vars *)param;
+	draw_3d_view(data);
+	draw_small_minimap(data);
+}
 
-// void	game_hook(void *param)
-// {
-// 	t_vars *data;
+void	game_hook(void *param)
+{
+	t_vars *data;
 
-// 	data = (t_vars *)param;
-// 	input_hook(data); // handle keyboard input.
-// 	mlx_cursor_hook(data->mlx, mouse_hook, data);
-// 	draw_hook(data); // draw minimap & draw 3d cast
-// }
-
+	data = (t_vars *)param;
+	input_hook(data); // handle keyboard input.
+	mlx_cursor_hook(data->mlx, mouse_hook, data);
+	draw_hook(data); // draw minimap & draw 3d cast
+}
 
 int	main(int argc, char **argv)
 {
@@ -40,19 +39,18 @@ int	main(int argc, char **argv)
 
 	if (argc != 2)
 		return (ft_strerror("Give 1 map argument please."));
-	// if (start_mlx(&data) == 1) //changed order with import_mapfile
-	// 	return (ft_strerror("Could not start mlx instance.\n"));
-	if (import_mapfile(&data, argv[1]) == 1) //maybe neat close mlx on error in here.
+	if (start_mlx(&data) == 1)
+		return (ft_strerror("Could not start mlx instance.\n"));
+	if (import_mapfile(&data, argv[1]) == 1)
+	{
+		printf("exit mlx maybe ?\n");
 		return (1);
-
-	// mlx_set_cursor_mode(data.mlx, MLX_MOUSE_HIDDEN); // move to init func
-	// mlx_loop_hook(data.mlx, &game_hook, &data);
-	// mlx_loop(data.mlx);
+	}
+	mlx_loop_hook(data.mlx, &game_hook, &data);
+	mlx_loop(data.mlx);
 
 	//end of program clean.
-	// mlx_terminate(data.mlx);
-
-	// add clean for textures here(end of program clean).
+	mlx_terminate(data.mlx);
 	clean_textures(&data);
 	clean_map_info(&data);
 	clean_array(data.themap);
