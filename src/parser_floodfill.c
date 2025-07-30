6f45fa6412fb2630db5297c8252a1b658cff9280
Jaime Ilustre <jaimeilustre@yahoo.com>
2025-07-30 22:06:56 +0200

    norminetted modified flood fill functions

--- src/parser_floodfill.c
+++ src/parser_floodfill.c
@@ -6,7 +6,7 @@
 /*   By: rhol <rhol@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/06/02 16:52:57 by rhol          #+#    #+#                 */
-/*   Updated: 2025/07/30 19:56:33 by jaimeilustr   ########   odam.nl         */
+/*   Updated: 2025/07/30 22:03:56 by jaimeilustr   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,59 +58,13 @@ static void	flood_fill(t_vars *data, int x, int y, int *retval)
 	flood_fill(data, x, y - 1, retval);
 }
 
-// previous 
-// static void	flood_fill(t_vars *data, int x, int y, int *retval)
-// {
-// 	int	width;
-
-// 	width = (int)ft_strlen(data->themap[y]);
-// 	if (x < 0 || y < 0 || y >= data->mapheight || x >= width)
-// 	{
-// 		// data->themap[y][x] = '5';
-// 		*retval = 1;
-// 		return ;
-// 	}
-// 	if (data->themap[y][x] != '0')
-// 		return ;
-// 	data->themap[y][x] = '2';
-// 	flood_fill(data, x + 1, y, retval);
-// 	flood_fill(data, x - 1, y, retval);
-// 	flood_fill(data, x, y + 1, retval);
-// 	flood_fill(data, x, y - 1, retval);
-// 	return ;
-// }
-
-// Wrapper for floodfill.
-// int	check_if_enclosed(t_vars *data)
-// {
-// 	int	retval;
-// 	int	flatx;
-// 	int	flaty;
-
-// 	retval = 0;
-// 	flatx = (int)data->plx;
-// 	flaty = (int)data->ply;
-// 	data->themap[flaty][flatx] = '0';
-// 	flood_fill(data, flatx, flaty, &retval);
-// 	return (retval);
-// }
-
-int	check_if_enclosed(t_vars *data)
+// Flood fill for unreachable space
+static int	check_unreachable_space(t_vars *data)
 {
-	int	retval;
-	int	flatx;
-	int	flaty;
 	int	y;
 	int	x;
 	int	region_retval;
 
-	flatx = (int)data->plx;
-	flaty = (int)data->ply;
-	retval = 0;
-	data->themap[flaty][flatx] = '0';
-	flood_fill(data, flatx, flaty, &retval);
-	if (retval == 1)
-		return (1);
 	y = 0;
 	while (data->themap[y] != NULL)
 	{
@@ -131,3 +85,21 @@ int	check_if_enclosed(t_vars *data)
 	return (0);
 }
 
+// Wrapper for floodfill.
+int	check_if_enclosed(t_vars *data)
+{
+	int	retval;
+	int	flatx;
+	int	flaty;
+
+	flatx = (int)data->plx;
+	flaty = (int)data->ply;
+	retval = 0;
+	data->themap[flaty][flatx] = '0';
+	flood_fill(data, flatx, flaty, &retval);
+	if (retval == 1)
+		return (1);
+	if (check_unreachable_space(data) == 1)
+		return (1);
+	return (0);
+}
