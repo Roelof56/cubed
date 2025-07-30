04df9589692c2b81dcd8e3830fe75bd69587c337
Jaime Ilustre <jaimeilustre@yahoo.com>
2025-07-30 15:27:31 +0200

    fixed overflow check

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,11 +6,12 @@
 /*   By: roelof <roelof@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/28 13:00:24 by roelof        #+#    #+#                 */
-/*   Updated: 2025/06/02 16:54:22 by rhol          ########   odam.nl         */
+/*   Updated: 2025/07/30 15:24:53 by jilustre      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <limits.h>
 
 int	ft_atoi(const char *str)
 {
@@ -21,8 +22,8 @@ int	ft_atoi(const char *str)
 	min = 1;
 	res = 0;
 	i = 0;
-	while (str[i] && (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' || \
-						str[i] == '\v' || str[i] == '\f' || str[i] == '\r'))
+	while (str[i] && (str[i] == ' ' || str[i] == '\n' || str[i] == '\t'
+			|| str[i] == '\v' || str[i] == '\f' || str[i] == '\r'))
 		str++;
 	if (str[i] == '-')
 		min = -1;
@@ -30,12 +31,12 @@ int	ft_atoi(const char *str)
 		str++;
 	while (str[i] && ft_isdigit(str[i]))
 	{
-		if (i >= 10 && min == 1)
-			return (-1);
-		if (i >= 10 && min == -1)
-			return (0);
 		res = res * 10 + (str[i] - 48);
+		if (min == 1 && res > INT_MAX)
+			return (-1);
+		if (min == -1 && - res < INT_MIN)
+			return (0);
 		i++;
 	}
-	return (res * min);
+	return ((int)(res * min));
 }
