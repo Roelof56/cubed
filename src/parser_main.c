e616d75d12afdca92b87ecb41622c6b8994480e6
rhol <rhol@student.codam.nl>
2025-07-31 18:57:23 +0200

    Remove error msg from error_clean

--- src/parser_main.c
+++ src/parser_main.c
@@ -6,7 +6,7 @@
 /*   By: rhol <rhol@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/06/02 16:52:45 by rhol          #+#    #+#                 */
-/*   Updated: 2025/07/31 18:11:36 by rhol          ########   odam.nl         */
+/*   Updated: 2025/07/31 18:56:37 by rhol          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,10 +44,8 @@ int	check_file_extension(char *str, char *ext)
 
 // handle cleaning for import_mapfile
 // from get_map_info & everything below it.
-static int	error_clean(t_vars *data, t_maplst **head, int len, char *str)
+static int	error_clean(t_vars *data, t_maplst **head, int len)
 {
-	if (str != NULL)
-		printf("Error\n%s\n", str);
 	ll_clean_list(head);
 	clean_textures(data);
 	clean_map_info(data);
@@ -64,22 +62,22 @@ int	import_mapfile(t_vars *data, char *str)
 
 	head = NULL;
 	if (check_file_extension(str, ".cub") == 1)
-		return (error_clean(data, &head, 0, NULL));
+		return (error_clean(data, &head, 0));
 	if (open_and_emptycheck(str, &fd) == 1)
 	{
 		clean_map_info(data);
 		return (1);
 	}
 	if (file_to_linkedlist(fd, &head, 0) == 1)
-		return (error_clean(data, &head, 0, NULL));
+		return (error_clean(data, &head, 0));
 	if (get_map_info(head, data) == 1)
-		return (error_clean(data, &head, 0, NULL));
+		return (error_clean(data, &head, 0));
 	if (load_that_map(data, head) == 1)
-		return (error_clean(data, &head, 0, NULL));
+		return (error_clean(data, &head, 0));
 	if (validate_that_map(data) == 1)
-		return (error_clean(data, &head, (ll_listsize(head) - 6), NULL));
+		return (error_clean(data, &head, (ll_listsize(head) - 6)));
 	if (make_map_square(data) == 1)
-		return (error_clean(data, &head, (ll_listsize(head) - 6), NULL));
+		return (error_clean(data, &head, (ll_listsize(head) - 6)));
 	ll_clean_list(&head);
 	return (0);
 }
