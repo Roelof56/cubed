6da62c0b0b990ff244029d1baef7cd736fb85a06
rhol <rhol@student.codam.nl>
2025-07-31 14:33:35 +0200

    Comment out empty file func

--- src/parser_main.c
+++ src/parser_main.c
@@ -6,7 +6,7 @@
 /*   By: rhol <rhol@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/06/02 16:52:45 by rhol          #+#    #+#                 */
-/*   Updated: 2025/07/31 13:03:39 by roelof        ########   odam.nl         */
+/*   Updated: 2025/07/31 14:31:17 by rhol          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,29 +57,29 @@ static int	error_clean(t_vars *data, t_maplst **head, int len, char *str)
 }
 
 // use retval instead of return. this way gnl finishes its loop and can't leak its buffer.
-static int	check_for_empty_file(int fd)
-{
-	char	*line;
-	int		i;
-	int		retval;
+// static int	check_for_empty_file(int fd)
+// {
+// 	char	*line;
+// 	int		i;
+// 	int		retval;
 
-	retval = 1;
-	line = get_next_line(fd);
-	i = 0;
-	while (line != NULL)
-	{
-		while (line[i])
-		{
-			if (line[i] != ' ' && line[i] != '\n')
-				retval = 0;
-		}
-		free(line);
-		line = get_next_line(fd);
-		i = 0;
-	}
-	close(fd);
-	return (retval);
-}
+// 	retval = 1;
+// 	line = get_next_line(fd);
+// 	i = 0;
+// 	while (line != NULL)
+// 	{
+// 		while (line[i])
+// 		{
+// 			if (line[i] != ' ' && line[i] != '\n')
+// 				retval = 0;
+// 		}
+// 		free(line);
+// 		line = get_next_line(fd);
+// 		i = 0;
+// 	}
+// 	// close(fd);
+// 	return (retval);
+// }
 
 // maybe move map square.
 int	import_mapfile(t_vars *data, char *str)
@@ -95,8 +95,8 @@ int	import_mapfile(t_vars *data, char *str)
 		printf("Error\nCan't open file\n");
 		return (error_clean(data, &head, 0, NULL));
 	}
-	if (check_for_empty_file(fd) == 1)
-		return (error_clean(data, &head, 0, "Empty file."));
+	// if (check_for_empty_file(fd) == 1)
+	// 	return (error_clean(data, &head, 0, "Empty file."));
 	if (file_to_linkedlist(fd, &head, 0) == 1)
 		return (error_clean(data, &head, 0, NULL));
 	if (get_map_info(head, data) == 1)
